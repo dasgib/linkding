7a17588995e2e0a7f705ceea8fed63d91820e9f6
Moritz Breit <mail@mobr.de>
2012-01-04 17:28:38 +0100

    Initial Rails project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkding::Application.config.secret_token = 'c595e2d7105c4512489b1e466b36ae40485469b80b8d7a2f5d139fdbee61e9fbe110d19fcb75fcb7dd093c01e4b696f93a0423f2770c13d48dac555e3c81bbe0'
