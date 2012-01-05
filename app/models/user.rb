92206cdbca52c034bf047e8546ce5a159100dcee
Moritz Breit <mail@mobr.de>
2012-01-05 17:06:59 +0100

    Add devise user model (#8)

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         # :omniauthable,
+         :timeoutable, :lockable, :token_authenticatable,
+         :confirmable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+end
