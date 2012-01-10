86b60e618732b388ac893fb6dfdd5987281c19d9
Moritz Breit <mail@mobr.de>
2012-01-10 21:30:37 +0100

    Ignore bookmarks with validation errors on import

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -6,14 +6,14 @@ class ImportController < ApplicationController
     dom = Nokogiri::HTML(params[:file].read)
     count = 0
     dom.xpath('//dt/a').each do |a|
-      Bookmark.create!(
+      bookmark = Bookmark.create(
           user: current_user,
           title: a.content,
           url: a['href'],
           tag_list: a['tags'],
           public: a['private'] == "0"
       )
-      count += 1
+      count += 1 unless bookmark.errors.present?
     end
 
     redirect_to bookmarks_path, notice: I18n.translate('import.success', count: count)
