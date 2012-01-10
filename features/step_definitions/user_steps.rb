1ba915b5180c61b240d3dc03d506e9133dc62bd3
Moritz Breit <mail@mobr.de>
2012-01-10 19:20:24 +0100

    Fix "I am a new, authenticated user" step definition

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -9,12 +9,12 @@ Given /^I am a user named "([^"]*)" with an email "([^"]*)" and password "([^"]*
 end
 
 Given /^I am a new, authenticated user$/ do
+  username = 'testing'
   email = 'testing@man.net'
-  login = 'Testing man'
   password = 'secretpass'
 
-  step %{I have one user "#{email}" with password "#{password}"}
-  step %{I go to login}
+  step %{I am a user named "#{username}" with an email "#{email}" and password "#{password}"}
+  step %{I go to the sign in page}
   step %{I fill in "user_email" with "#{email}"}
   step %{I fill in "user_password" with "#{password}"}
   step %{I press "Sign in"}
