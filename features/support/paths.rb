df23f5c645a5be3fd1c05db93104b0db8558c833
Moritz Breit <mail@mobr.de>
2012-01-10 19:24:43 +0100

    Add the new bookmark page to cucumber paths #22

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -25,6 +25,8 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
+    when /the new bookmark page/
+      '/bookmarks/new'
 
     else
       begin
