15e6fa5767bcc0b697366963c7924736decc73ed
Moritz Breit <mail@mobr.de>
2012-01-10 21:01:37 +0100

    Fix n+1 queries in bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,4 +9,10 @@ class BookmarksController < InheritedResources::Base
     @bookmark = current_user.bookmarks.build(params[:bookmark])
     create! { bookmarks_path }
   end
+
+  protected
+
+  def collection
+    @bookmarks ||= end_of_association_chain.includes(:site).includes(:tags).paginate(page: params[:page], per_page: 10)
+  end
 end
