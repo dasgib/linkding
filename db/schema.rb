e5ce78acabe54ea532935f6c33e59a47988b206c
Moritz Breit <mail@mobr.de>
2012-01-10 19:38:15 +0100

    Add site model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120109175119) do
+ActiveRecord::Schema.define(:version => 20120109184516) do
+
+  create_table "sites", :force => true do |t|
+    t.string   "url",        :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username",               :limit => 20
