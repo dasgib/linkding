23d9d3918eb6f84b790376c57a77d1508b7900c8
Moritz Breit <mail@mobr.de>
2012-01-11 17:00:49 +0100

    Add a list of active tags to the sidebar of the bookmark list
    Note: This requires a layout with a sidebar content section, which
    the current layout does not have yet. Without a sidebar section, the
    tag list will not be visible.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,6 +2,12 @@ class BookmarksController < InheritedResources::Base
 
   has_scope :tagged_with, as: 'tag'
 
+  def index
+    # TODO: refactor tag list into a cell as soon as we have Cells
+    @tags = current_user.active_tags.limit(50)
+    index!
+  end
+
   def create
     create! { bookmarks_path }
   end
