2b4c24b815dcd9c4188f7a38d6d0b51326d8590c
Moritz Breit <mail@mobr.de>
2012-01-11 14:42:52 +0100

    Add simple bookmarklet #24

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     base_url = https ? "https://secure.gravatar.com" : "http://www.gravatar.com"
     image_tag "#{base_url}/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options
   end
+
+  def bookmarklet
+    location = "encodeURIComponent(window.location)"
+    title = "encodeURIComponent(window.document.title)"
+    url = %{"#{new_bookmark_url}?bookmark[url]=" + #{location} + "&bookmark[title]=" + #{title}}
+
+    window_options = "width=600,height=400,status=yes,scrollbars=yes,resizable=yes"
+
+    link_to "Bookmarklet", %{javascript:open(#{url}, "bookmark", "#{window_options}").focus();}
+  end
 end
