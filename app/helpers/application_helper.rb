ec5d7ebc16fd4f9be9986e4d9dd8c2b571343c17
Moritz Breit <mail@mobr.de>
2012-01-11 14:42:52 +0100

    Add simple bookmarklet #24

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def bookmarklet
+    location = "encodeURIComponent(window.location)"
+    title = "encodeURIComponent(window.document.title)"
+    url = %{"#{new_bookmark_url}?bookmark[url]=" + #{location} + "&bookmark[title]=" + #{title}}
+
+    window_options = "width=600,height=400,status=yes,scrollbars=yes,resizable=yes"
+
+    link_to "Bookmarklet", %{javascript:open(#{url}, "bookmark", "#{window_options}").focus();}
+  end
 end
