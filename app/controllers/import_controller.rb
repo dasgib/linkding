0f76fbd2ab5248351aca0277efbe9f18249539c4
Moritz Breit <mail@mobr.de>
2012-01-11 13:53:46 +0100

    Import: handle exceptions and import add_date, redirect after import

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -4,18 +4,31 @@ class ImportController < ApplicationController
 
   def create
     dom = Nokogiri::HTML(params[:file].read)
-    count = 0
+    success_count = 0
+    failure_count = 0
     dom.xpath('//dt/a').each do |a|
-      bookmark = Bookmark.create(
-          user: current_user,
-          title: a.content,
-          url: a['href'],
-          tag_list: a['tags'],
-          public: a['private'] == "0"
-      )
-      count += 1 unless bookmark.errors.present?
+      begin
+        Bookmark.create!(
+            user: current_user,
+            title: a.content,
+            url: a['href'],
+            tag_list: a['tags'],
+            public: a['private'] == "0",
+            created_at: Time.at(a['add_date'].to_i)
+        )
+        success_count += 1
+      rescue Exception => e
+        failure_count += 1
+        logger.info(e)
+      end
     end
 
-    redirect_to bookmarks_path, notice: I18n.translate('import.success', count: count)
+    notice = if failure_count == 0
+      I18n.translate('import.success', success_count: success_count)
+    else
+      I18n.translate('import.skipped', success_count: success_count, failure_count: failure_count)
+    end
+
+    redirect_to bookmarks_path, notice: notice
   end
 end
