8435327666cc06355fe0a5370ae037a872fbf716
Moritz Breit <mail@mobr.de>
2012-01-11 17:25:08 +0100

    Add avatar helper method (which displays the gravatar for now)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def avatar(user, options = {})
+    size = options.delete(:size) || 40
+    image_tag "http://www.gravatar.com/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options
+  end
 end
