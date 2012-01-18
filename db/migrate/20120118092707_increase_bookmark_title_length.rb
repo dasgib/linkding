d7df304a24622761d7211b6330c9718898a0e104
Moritz Breit <mail@mobr.de>
2012-01-18 10:50:28 +0100

    Increase bookmark title length to 1024

---
+++ db/migrate/20120118092707_increase_bookmark_title_length.rb
@@ -0,0 +1,9 @@
+class IncreaseBookmarkTitleLength < ActiveRecord::Migration
+  def up
+    change_column :bookmark, :title, :string, null: false, limit: 1024
+  end
+
+  def down
+    change_column :bookmark, :title, :string, null: false, limit: 255
+  end
+end
