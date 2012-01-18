4fd2044ff4222108d0764d91e4d6dd4437c294ea
Moritz Breit <mail@mobr.de>
2012-01-18 11:09:25 +0100

    Fix increase_bookmark_title_length migration: typo on table name

--- db/migrate/20120118092707_increase_bookmark_title_length.rb
+++ db/migrate/20120118092707_increase_bookmark_title_length.rb
@@ -1,9 +1,9 @@
 class IncreaseBookmarkTitleLength < ActiveRecord::Migration
   def up
-    change_column :bookmark, :title, :string, null: false, limit: 1024
+    change_column :bookmarks, :title, :string, null: false, limit: 1024
   end
 
   def down
-    change_column :bookmark, :title, :string, null: false, limit: 255
+    change_column :bookmarks, :title, :string, null: false, limit: 255
   end
 end
