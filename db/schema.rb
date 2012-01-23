44dbe29af8a6e773c2d5a85ef8f1f09c96409c55
Moritz Breit <mail@mobr.de>
2012-01-23 16:47:08 +0100

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120117231848) do
+ActiveRecord::Schema.define(:version => 20120118092707) do
 
   create_table "bookmarks", :force => true do |t|
-    t.integer  "site_id",                        :null => false
-    t.integer  "user_id",                        :null => false
-    t.string   "title",                          :null => false
-    t.boolean  "public",      :default => false, :null => false
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.integer  "site_id",                                        :null => false
+    t.integer  "user_id",                                        :null => false
+    t.string   "title",       :limit => 1024,                    :null => false
+    t.boolean  "public",                      :default => false, :null => false
+    t.datetime "created_at",                                     :null => false
+    t.datetime "updated_at",                                     :null => false
     t.text     "description"
     t.integer  "import_id"
   end
