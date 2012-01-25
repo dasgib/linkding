f428b8fa2b069adf128012dab49391c5ffff1d64
Moritz Breit <mail@mobr.de>
2012-01-25 14:18:42 +0100

    Deactivate user sign up functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,# :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          # :omniauthable,
          :timeoutable, :lockable, :token_authenticatable,
