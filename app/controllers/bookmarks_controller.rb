00ea45bffe242a9e037e478211782d759c50845b
Moritz Breit <mail@mobr.de>
2012-01-25 23:14:14 +0100

    BookmarksController: use begin_of_association_chain again

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,11 +4,6 @@ class BookmarksController < InheritedResources::Base
   has_scope :tagged_with, as: 'tag'
   has_scope :page, default: 1, only: [:index, :recent]
 
-  def create
-    @bookmark = current_user.bookmarks.build(params[:bookmark])
-    create!
-  end
-
   def index
     # TODO: refactor tag list into a cell as soon as we have Cells
     @tags = current_user.active_tags.limit(50)
@@ -16,7 +11,7 @@ class BookmarksController < InheritedResources::Base
   end
 
   def recent
-    @bookmarks = collection.public
+    @bookmarks = apply_scopes(Bookmark).public.includes(:site, :tags).order("created_at desc")
     @tags = Bookmark.public.active_tags.limit(50)
   end
 
@@ -24,5 +19,9 @@ class BookmarksController < InheritedResources::Base
 
   def collection
     @bookmarks ||= end_of_association_chain.includes(:site, :tags).order("created_at desc")
+  end
+
+  def begin_of_association_chain
+    current_user
   end
 end
