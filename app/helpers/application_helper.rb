7a1981a1fb3eeb682901d6a06e748512388542e7
Moritz Breit <mail@mobr.de>
2012-01-25 23:50:42 +0100

    Use gravatar over  https

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def avatar(user, options = {})
     size = options.delete(:size) || 40
-    image_tag "http://www.gravatar.com/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options
+    https = options.delete(:https)
+    base_url = https ? "https://secure.gravatar.com" : "http://www.gravatar.com"
+    image_tag "#{base_url}/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options
   end
 end
