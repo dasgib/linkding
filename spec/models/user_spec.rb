84af5dc86f2ed1741cc0a29498167726d7b85f15
Moritz Breit <mail@mobr.de>
2012-03-27 14:59:07 +0200

    Add specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Factory.create(:user) }
+
+  describe "#active_tags" do
+    it { should have(0).active_tags }
+
+    context "with one bookmark with tag foobar" do
+      before(:each) do
+        subject.bookmarks.create(title: "test", url: "http://example.com", tag_list: "foobar")
+      end
+
+      it { should have(1).active_tags }
+      its(:active_tags) { should include(ActsAsTaggableOn::Tag.find_by_name("foobar")) }
+    end
+  end
 end
