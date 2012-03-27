8f65087e087eb301aac52ee2c716cd86e51a6b97
Moritz Breit <mail@mobr.de>
2012-03-27 17:21:39 +0200

    Clean up spec_helper and use truncation strategy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,14 +38,6 @@ Spork.prefork do
     # config.mock_with :rr
     config.mock_with :rspec
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    #config.use_transactional_fixtures = true
-
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
@@ -53,11 +45,9 @@ Spork.prefork do
 
     config.include FactoryGirl::Syntax::Methods
 
-
     # Set up database_cleaner
     config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
+      DatabaseCleaner.strategy = :truncation
     end
 
     config.before(:each) do
