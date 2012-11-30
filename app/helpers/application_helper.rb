e71ef1a46b6fc3f3c64b9ff19a2cd61160dbecd7
Moritz Breit <mail@mobr.de>
2012-11-30 16:50:33 +0100

    First parts of new layout
    - Add gray header bar with a subtle gradient
    - Make main menu horizontal
    - Move search form to sidebar
    - Move sass variable definitions to base.css.sass
    - Refactor responsive.css.sass
    - Move main menu below logo on small width layouts
    - Hide label and submit button in search form
    - Build a mobile layout with a working and usable header and main menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar(user, options = {})
-    size = options.delete(:size) || 40
+    size = options.delete(:size) || 30
     https = options.delete(:https)
     base_url = https ? "https://secure.gravatar.com" : "http://www.gravatar.com"
     image_tag "#{base_url}/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options
