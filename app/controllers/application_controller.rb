8fc9fbbde42a321eb26dac92e2fcb1f1bc288abb
Moritz Breit <mail@mobr.de>
2012-12-03 17:18:36 +0100

    Add last_modified helper functions to ApplicationController
    The last_modified helper function returns a last modified timestamp
    based on the parameters, the deployment time and the current user object.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,34 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
 
+protected
+
+  def last_modified(*objects)
+    last_modified_for_object([last_modified_objects, objects])
+  end
+
+  def last_modified_objects
+    [current_user, deployment_time]
+  end
+
+  def last_modified_for_object(object)
+    case object
+    when Time, ActiveSupport::TimeWithZone
+      object
+    when ActiveRecord::Relation
+      object.limit(nil).maximum(:updated_at)
+    when Array
+      object.map { |o| last_modified_for_object(o) }.max
+    else
+      if object.respond_to? :updated_at
+        object.updated_at
+      else
+        raise "Unsupported type in last_modified_for_object: #{object.class}"
+      end
+    end
+  end
+
+  def deployment_time
+    Time.rfc2822(ENV['DEPLOYMENT_TIME'])
+  end
 end
