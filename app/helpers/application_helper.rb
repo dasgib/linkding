d9b9c55c351183829ca128067fdbab88c5e59bd4
Moritz Breit <mail@mobr.de>
2012-12-06 14:59:57 +0100

    Avatar-Styling: Add empty alt tag, define fixed size and retina compatibility

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def avatar(user, options = {})
-    size = options.delete(:size) || 30
+    size = options.delete(:size) || 60
     https = options.delete(:https)
     base_url = https ? "https://secure.gravatar.com" : "http://www.gravatar.com"
-    image_tag "#{base_url}/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options
+    image_tag "#{base_url}/avatar/#{Digest::MD5.new.hexdigest(user.email.downcase)}?s=#{size}", options.merge(alt: "")
   end
 end
